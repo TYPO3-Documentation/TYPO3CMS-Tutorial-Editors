bb852156d5899e9a5f0c70133b2b57e9ee4e50fe
Christopher <chrissitopher@gmx.de>
2012-05-23 15:33:20 +0200

    [TASK] Migrate doc_tut_editors 0.0.6
    Import doc_tut_editors 0.0.6.
    
    Change-Id: I110e891e0d71b5a5ccb03921985cd7ccc5633a44

---
+++ doc_tut_editors/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "typo3_tut"
+#
+# Auto generated 31-07-2008 23:24
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Typo3 Tutorial',
+	'description' => 'This tutorial explains how to add or change content on an existing website in Typo3. It provides the most basic instructions to get the job done with step by step instructions. Only the information relevant to get the job is provided, no in depth explanation of all other available options in Typo3 is given.',
+	'category' => 'doc',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Marlies Cohen',
+	'author_email' => 'mc@mcuniverse.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.0.6',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"doc/manual.sxw";s:4:"d221";s:19:"doc/wizard_form.dat";s:4:"e579";s:20:"doc/wizard_form.html";s:4:"6b59";}',
+);
+
+?>
