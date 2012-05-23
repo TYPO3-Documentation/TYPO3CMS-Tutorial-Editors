0e997a007bc00f6cfdf029699e2fe1c39687f31c
Christopher <chrissitopher@gmx.de>
2012-05-23 15:37:35 +0200

    [TASK] Migrate doc_tut_editors 1.1.1
    Import doc_tut_editors 1.1.1.
    
    Change-Id: I080975c17d4fc664db737d142e3418eff4f343c2

--- doc_tut_editors/ext_emconf.php
+++ doc_tut_editors/ext_emconf.php
@@ -1,13 +1,13 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "typo3_tut"
+# Extension Manager/Repository config file for ext "typo3_tut".
 #
-# Auto generated 31-07-2008 23:24
+# Auto generated 22-08-2011 16:54
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
@@ -15,34 +15,34 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This tutorial explains how to add or change content on an existing website in TYPO3. It provides the most basic instructions to get the job done with step by step instructions. Only the information relevant to get the job is provided, no in depth explanation of all other available options in TYPO3 is given.',
 	'category' => 'doc',
 	'shy' => 0,
+	'version' => '1.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
 	'module' => '',
-	'state' => 'stable',
+	'state' => 'obsolete',
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 0,
 	'lockType' => '',
 	'author' => 'DocTeam, Susanne Moog',
 	'author_email' => 'typo3@susannemoog.de',
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '3.5.0-0.0.0',
 			'php' => '4.0.0-0.0.0',
+			'typo3' => '3.5.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"doc/manual.sxw";s:4:"d221";s:19:"doc/wizard_form.dat";s:4:"e579";s:20:"doc/wizard_form.html";s:4:"6b59";}',
+	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"45af";s:12:"ext_icon.gif";s:4:"b4e6";s:14:"doc/manual.pdf";s:4:"9c8b";s:14:"doc/manual.sxw";s:4:"8a9b";s:19:"doc/wizard_form.dat";s:4:"e579";s:20:"doc/wizard_form.html";s:4:"6b59";}',
 );
 
 ?>
